2279fbd03a4481c79e1cbd535935b8007125ed3b
Aditi-panta <88805173+Aditi-panta@users.noreply.github.com>
2022-08-27 14:48:59 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -10,4 +10,19 @@ recognition.onresult=function(event){
     var content=event.results[0][0].transcript;
     document.getElementById("textbox").innerHTML=content;
     console.log(content);
+    speak()
 }
+function speak(){
+    var synth=window.speechSynthesis;
+    speak_data=document.getElementById("textbox").value ;
+    var utterthis=new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterthis);
+    Webcam.attach(camera);
+}
+Webcam.set({
+    width:360,
+    height:250,
+    image_format:'png',
+    png_quality:90
+});
+camera=document.getElementById("camera");
