9cacb68de87a441c4b4932b99814eea6df7afc93
Aditi-panta <88805173+Aditi-panta@users.noreply.github.com>
2022-09-03 14:46:23 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -10,14 +10,27 @@ recognition.onresult=function(event){
     var content=event.results[0][0].transcript;
     document.getElementById("textbox").innerHTML=content;
     console.log(content);
-    speak()
+    if(content=="take my selfie"){
+        speak();
+    }
+    
+
 }
 function speak(){
     var synth=window.speechSynthesis;
-    speak_data=document.getElementById("textbox").value ;
+    speak_data="taking your selfie in five seconds" ;
     var utterthis=new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterthis);
     Webcam.attach(camera);
+    setTimeout(function(){
+        take_snapshot();
+    },5000);
+    setTimeout(function(){
+        take_snapshot1();
+    },10000);
+    setTimeout(function(){
+        take_snapshot2();
+    },15000);
 }
 Webcam.set({
     width:360,
@@ -26,3 +39,18 @@ Webcam.set({
     png_quality:90
 });
 camera=document.getElementById("camera");
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="selfie_image" src="'+ data_uri+'">';
+    });
+}
+function take_snapshot1(){
+    Webcam.snap(function(data_uri1){
+        document.getElementById("result1").innerHTML='<img id="selfie_image1" src="'+ data_uri1+'">';
+    });
+}
+function take_snapshot2(){
+    Webcam.snap(function(data_uri2){
+        document.getElementById("result2").innerHTML='<img id="selfie_image2" src="'+ data_uri2+'">';
+    });
+}
